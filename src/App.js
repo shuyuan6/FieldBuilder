a2a34d6d9a938abfaaff0995dc3ae0d4e0877470
Shuyuan Lyu <sylyu6@gmail.com>
2021-03-01 00:49:22 -0800

    Minor refactor

--- src/App.js
+++ src/App.js
@@ -169,7 +169,6 @@ class App extends React.Component {
   render() {
     return (
     <div className='container'>
-      
       <div className='fieldBuilder'>
         Field Builder
       </div>
